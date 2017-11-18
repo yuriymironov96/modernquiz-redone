c477cb674423cda4697700981075b6db53ba2e6f
Yuriy Mironov <yuriymironov96@gmail.com>
2017-11-18 19:26:49 +0200

    Add models to quiz app

--- modernquiz/modernquiz/dummy.settings.py
+++ modernquiz/modernquiz/dummy.settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'django_filters'
+    'django_filters',
+
+    'quizes',
 ]
 
 MIDDLEWARE = [
--- modernquiz/quizes/models.py
+++ modernquiz/quizes/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 
+class Quiz(models.Model):
+    title = models.CharField(
+        max_length=200
+    )
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
+    create_time = models.DateTimeField(
+        auto_now_add=True
+    )
+    update_time = models.DateTimeField(
+        auto_now=True
+    )
+
+
+class Question(models.Model):
+    QUESTION_TYPES = (
+        ('single_choice', 'Single choice'),
+        ('multiple_choice', 'Multiple choice')
+    )
+    quiz = models.ForeignKey(
+        'quizes.Quiz',
+        on_delete=models.CASCADE,
+        related_name='questions',
+    )
+    question_type = models.CharField(
+        choices=QUESTION_TYPES,
+        default='single_choice',
+        max_length=50
+    )
+    question_text = models.TextField()
+    
+
+class Choice(models.Model):
+    question = models.ForeignKey(
+        'quizes.Question',
+        on_delete=models.CASCADE
+    )
+    is_correct = models.BooleanField(
+        default=False
+    )
+    choice_text = models.TextField()
