d63e28e2078852083145444dfe54abe1a4479eb5
Yuriy Mironov <yuriymironov96@gmail.com>
2017-12-08 02:28:18 +0200

    Add frontend routing

--- client/modernquiz/src/app/app.module.ts
+++ client/modernquiz/src/app/app.module.ts
@@ -1,11 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { NavigationModule } from './navigation/navigation.module';
+
+import { CommonDashboardModule } from './common-dashboard/common-dashboard.module';
+import { HomePageComponent } from './common-dashboard/components/home-page/home-page.component';
+
 import { TeacherDashboardModule } from './teacher-dashboard/teacher-dashboard.module';
+import { QuizUploadComponent } from './teacher-dashboard/components/quiz-upload/quiz-upload.component';
 
 import { AppComponent } from './app.component';
 
+
+export const ROUTES: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'import',
+    component: QuizUploadComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -13,7 +33,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(ROUTES),
     NavigationModule,
+    CommonDashboardModule,
     TeacherDashboardModule
   ],
   providers: [],
--- client/modernquiz/src/app/navigation/navigation.module.ts
+++ client/modernquiz/src/app/navigation/navigation.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { MqNavbarComponent } from './components/mq-navbar/mq-navbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     NoopAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   declarations: [MqNavbarComponent],
   exports: [MqNavbarComponent]
